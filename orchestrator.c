a75f146d70bbb1d9df258e7a2091bf3eb3736999
Arjuna Maudhoo <apm526@nyu.edu>
2026-09-15 09:34:10 -0500

    Finish orchestrator layer

--- orchestrator.c
+++ orchestrator.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
 
 
 #include "pipeline.h"
@@ -37,11 +40,101 @@ int main (int argc, char *argv[])
   pid_t worker_pids[MAX_WORKERS];
   int worker_fds[MAX_WORKERS];
 
+  // spawning the workers, need unique socket path, child process running the actual worker binary and reuslting PID saved
 
+  for (int i = 0; i < num_workers; i++)
+  {
+    snprintf(worker_paths[i], sizeof(worker_paths[i]), "/tmp/pipeline_worker_%d.sock", i);
 
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+      perror("fork");
+      return 1;
+    }
+    if (pid == 0) // only true in the new child
+    {
+      // replaces current process image with a new process image
+      execl("./worker", "worker", "-l", worker_paths[i], "-c", sink_path, (char *)NULL);
+      perror("execl");
+      _exit(1);
+    }
+    worker_pids[i] = pid;
+    printf("[orchestrator] spawned wokrer %d (pid=%d) listening at %s\n", i, pid, worker_paths[i]);
+  }
 
+  // connect to each spawned worker
+  for (int i = 0; i < num_workers; i++)
+  {
+    worker_fds[i] = unix_connect_retry(worker_paths[i], 20, 100000);
+    if (worker_fds[i] < 0)
+    {
+      fprintf(stderr, "[orchestrator] could not connect to worker %d\n", i);
+      return 1;
+    }
+    printf("[orchestrator] connected to worker %d\n", i);
+
+  }
 
   // accept frames from sensor-sim
-  // round-robin dispatch each frame to one of the N workers
+  int listen_fd = unix_listen(listen_path);
+  if (listen_fd < 0)
+  {
+    fprintf(stderr, "[orchestrator] could not listen on %s\n", listen_path);
+    return 1;
+  }
+  printf("[orchestrator] listening for sensor-sim on %s\n", listen_path);
   
+  int client_fd = accept(listen_fd, NULL, NULL);
+  if (client_fd < 0)
+  {
+    perror("accept");
+    return 1;
+  }
+  printf("[orchestrator] sensor-sim connected, dispatching frames\n");
+
+
+  // round-robin dispatch each frame to one of the N workers
+  uint64_t dispatched = 0;
+  int next_worker = 0;
+  frame_t frame;
+  for (;;)
+  {
+    ssize_t r = read_full(client_fd, &frame, sizeof(frame));
+    if (r == 0)
+    {
+      printf("[orchestrator] sensor-sim disconnected (EOF)\n");
+      break;
+    }
+    if (r != (ssize_t)sizeof(frame))
+    {
+      fprintf(stderr, "[orchestrator] short read/error from sensor-sim\n");
+      break;
+    }
+
+    ssize_t w = write_full(worker_fds[next_worker], &frame, sizeof(frame));
+    if (w != (ssize_t)sizeof(frame))
+    {
+      fprintf(stderr, "[orchestrator] short write to worker %d\n", next_worker);
+      break;
+    }
+
+    dispatched++;
+    next_worker = (next_worker + 1) % num_workers;
+  }
+
+  printf("[orchestrator] dispatched %llu frames total \n", (unsigned long long) dispatched);
+
+  close(client_fd);
+  close(listen_fd);
+  for (int i = 0; i < num_workers; i++)
+  {
+    close(worker_fds[i]);
+  }
+  // process has finished running, collecting entry sitting in kernel's proces table
+  for (int i = 0; i < num_workers; i++)
+  {
+    waitpid(worker_pids[i], NULL, 0);
+  }
+
 }
